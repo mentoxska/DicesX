fa4655529f982b37f156a7fd59bd4c8144d64db1
Branislav <mentox82@gmail.com>
2017-10-25 19:48:58 +0200

    Initial Commit

---
+++ DicesX by BranTheCoder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DicesX by BranTheCoder
+//
+//  Created by Branislav on 25/10/2017.
+//  Copyright © 2017 Branislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
